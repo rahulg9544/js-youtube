d2ad7fb8351f158d5c67c4b9d54e7ab537909968
rahulg9544 <rahulkrishnanrohith@gmail.com>
2024-10-23 14:41:41 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("rahul")
 
 
-let name = "hitesh"
+let name = "rahul"
 let age = 18
 let isLoggedIn = false
 let state;
